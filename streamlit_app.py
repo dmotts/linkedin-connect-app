4b133ed926a6599e36168fb033429fa45d89dcbf
Daley Mottley <daley.mottley@hotmail.com>
2024-04-09 22:23:36 +0000

    Switch to headless mode

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ import time
 def init_driver():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
+
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-extensions')
